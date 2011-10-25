0e08f2c9ecc4f5d907201a7510fedb354c498e20
sylvanaar <sylvanaar@mindspring.com>
2011-10-25 14:52:30 -0400

    fix the request URL

--- src/org/bitbucket/connectors/jetbrains/BitbucketUtil.java
+++ src/org/bitbucket/connectors/jetbrains/BitbucketUtil.java
@@ -58,7 +58,7 @@ public class BitbucketUtil {
 
 
     public static Element request(String username, String password, String url, boolean post, @Nullable Map<String, String> params, @Nullable String query) throws IOException, JDOMException {
-        url = "https://api." + url + "?format=xml";
+        url = "https://api." + BITBUCKET_DN + "/1.0" + url + "?format=xml";
 
         if (query != null)
             url = url + "&" + query;
